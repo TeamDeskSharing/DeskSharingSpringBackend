8356b13006b3e5cd131635860b84c359fad43ffb
jonathan <jonathan.gurk@hs-osnabrueck.de>
2021-12-29 18:19:05 +0100

    Office Init Plus Repo And Serive

--- src/main/java/com/example/demo/building/WorkplaceRepository.java
+++ src/main/java/com/example/demo/building/WorkplaceRepository.java
@@ -1,2 +1,8 @@
-package com.example.demo.building;public interface WorkplaceRepository {
+package com.example.demo.building;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WorkplaceRepository extends JpaRepository<Workplace, Long> {
+    Workplace findByphone(String phone);
 }
--- src/main/java/com/example/demo/building/WorkplaceService.java
+++ src/main/java/com/example/demo/building/WorkplaceService.java
@@ -1,2 +1,49 @@
-package com.example.demo.building;public class WorkplaceService {
+package com.example.demo.building;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Service("api/v1/workplaces")
+public class WorkplaceService {
+
+    @Autowired // new
+    private final WorkplaceRepository workplaceRepository;
+
+    @GetMapping
+    public List<Workplace> getAllWorkplaces(){
+        return workplaceRepository.findAll();
+    }
+
+    @PostMapping
+    public Workplace saveWorkplace(Workplace workplace){ //funktioniert auch als list
+        return workplaceRepository.save(workplace);
+    }
+
+    @GetMapping
+    public Workplace getWorkplace(long id){
+        return workplaceRepository.findById(id).orElse(null);
+    }
+
+    @GetMapping
+    public Workplace getWorkplaceByPhone(String phone){
+        return workplaceRepository.findByphone(phone);
+    }
+
+    public String deleteEmployee(long id){
+        workplaceRepository.deleteById(id);
+        return "employee deleted";
+    }
+
+    public Workplace updateWorkplace(Workplace workplace){
+        Workplace workplace1  = workplaceRepository.findById(workplace.getId()).orElse(null);
+        workplace1.setPhone(workplace.getPhone());
+        return workplaceRepository.save(workplace1);
+    }
 }
